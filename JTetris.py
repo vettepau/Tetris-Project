0d83c6f2b52d31732ae0de7ac0be651c57120a9c
vettepau <paulvetter99@gmail.com>
2020-10-15 22:22:10 -0500

    Initial commit

---
+++ JTetris.py
@@ -0,0 +1,17 @@
+'''
+Tetris Project 
+Creators:
+    Paul Vetter, Seth Webb, Anna Woodruff, & Sarah Rosinbaum
+Engr 102 final project
+
+The game will consist of 6 different classes:
+    Piece - a single tetris peice 
+    Board - the tetris board
+    JTester - tester class for Piece and Board
+    JTetris - present the GUI for tetris in a window and do animation
+    Brain - simple heuristic logic that knows how to play the tetris
+    JBrainTetris - a subclass of JTetris that uses a brain to play the game w/ out a human player
+    BrainTester - Possibly include this class to test our brain and implement machine learning
+
+Using Tetris-Architecture.html for guidance and resources 
+'''

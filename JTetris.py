2f47c4a7a54d8256cc3ec7c40b327abbc074b1ac
vettepau <paulvetter99@gmail.com>
2020-11-02 19:10:59 -0600

    updated layout

--- JTetris.py
+++ JTetris.py
@@ -7,9 +7,7 @@ Engr 102 final project
 The game will consist of 6 different classes:
     Piece - a single tetris peice 
     Board - the tetris board
-    JTester - tester class for Piece and Board
     JTetris - present the GUI for tetris in a window and do animation
-    
     Brain - simple heuristic logic that knows how to play the tetris
     JBrainTetris - a subclass of JTetris that uses a brain to play the game w/ out a human player
     BrainTester - Possibly include this class to test our brain and implement machine learning

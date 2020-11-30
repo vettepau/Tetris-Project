c22909054552fa077a8505513f200d38c68f21e7
vettepau <paulvetter99@gmail.com>
2020-11-30 02:44:09 -0600

    working brain
    The brain is horrible but it works and could function a lot better with some more time.

--- Brain.py
+++ Brain.py
@@ -155,7 +155,7 @@ def convertShape(shape):
 
 def valid(shape, grid):
     #The ifgrid[i][j] == black checks if empty space, we only want to add empty spaces to our valid list, for obvious reasons.
-    acceptedPositions = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0) ] for i in range(20)] #A tuple of all accepted positions allowed in the grid. Did it this way was tired of nested for loops, sue me. 
+    acceptedPositions = [[(j, i) for j in range(10) if grid[i][j] == 0 ] for i in range(20)] #A tuple of all accepted positions allowed in the grid. Did it this way was tired of nested for loops, sue me. 
     acceptedPositions = [j for sub in acceptedPositions for j in sub] #Had to look up how to do this, turns matrix into a list, 2D to 1D Emailed Ritchey to ask about will see when she responds
     """
     [[1,2] , [3,4]]
@@ -167,9 +167,8 @@ def valid(shape, grid):
     
     for position in converted:
         if position not in acceptedPositions: #Checks list of accepted positions
-            if position[1] > -1: #Incase Piece starts above the screen, otherwise the game would end before it started and thats no good. Would help with potentially needed offsets
-                return False #I KEEP WANTING TO PUT SEMI COLONS AFTER RETURN AHHHHHHHHHHHHHHHHHHH
-    return True #Because that means all must be true, all passed.
+            return False 
+    return True 
 
 def sort(grid):
     '''
@@ -182,19 +181,23 @@ def sort(grid):
     for k in range(len(grid)):
         for j in range(len(grid[k])):
             a,b,c = grid[k][j]
-            if a and b and c != 0:
+            if a or b or c != 0:
                 pos[k][j] = 1
     return pos
 
 def validPositions(piece, grid):
-    pos = [[]]
+    pos = []
+    counter = 0
     for i in range(10): #x cooridate of piece
-        h = 20 - getColumnHeight(grid,i) #lowest possible y coorindate of piece
-        for j in range(h,-1): #the board starts from -1 at the top and goes down to 20
+        h = 21 - getColumnHeight(grid,i) #lowest possible y coorindate of piece
+        for j in range(h+1): #the board starts from -2 at the top and goes down to 20
+            k = h - j
             piece.x = i
-            piece.y = j
+            piece.y = k #first piece should be 20
             if valid(piece, grid):
-                pos += [i,j]
+                pos.append([])
+                pos[counter] = [i,k]
+                counter += 1
                 break
     return pos #max length 10 with inside array of two ints
 
@@ -202,11 +205,11 @@ def getColumnHeight(grid, column):
     '''
     returns the height of the column
     '''
-    h = 0
+    h = 20
     c = column
-    for i in range(10):
-        if grid[i][c] == 1:
-            h = i + 1
+    for i in range(20):
+        if grid[i][c] == 0:
+            h -= 1
     return h
 
 def Heights(grid):
@@ -227,7 +230,7 @@ def clear(grid, maxx):
     returns number of lines cleared
     '''
     lines = 0
-    for i in range(maxx):
+    for i in range((20-maxx),20):
         num = 0
         for j in range(10):
             num += grid[i][j]
@@ -236,22 +239,24 @@ def clear(grid, maxx):
         num = 0
     return lines
 
-def rate(grid):
+def rate(grid,loc):
     '''
     rates the position based of criteria and returns a score
     '''
     maxx, avg = Heights(grid)
-    clear = (grid, maxx)
+    c = clear(grid, maxx)
     hole = 0
     for i in range(10):
-        h = getColumnHeight(grid,i) - 2 #where the first hole could be
-        for j in range(h+1):
+        h = 19 - getColumnHeight(grid,i) + 2 #where the first hole could be
+        if h > 19: #if there is no blocks on the board or only one level of blocks
+            continue
+        for j in range(h,20):
             if grid[j][i] == 0:
                 hole += 1
-    score = 8*maxx + 40*avg + 1.25*hole - 40*clear #random values that will be perfected later with machine learning
+    score = 8*maxx + 40*avg + 1.25*hole - 40*c #random values that will be perfected later with machine learning
     return score
     
-                
+import copy as c
 def best_position(piece,grid):
     '''
     tests the positions that are valid and decides which one is the best.
@@ -261,14 +266,18 @@ def best_position(piece,grid):
     best_x = 0
     pos = validPositions(piece, grid)
     for i in range(len(pos)):
-        grid1 = grid #new grid for each possible location
+        grid1 = c.deepcopy(grid) #this cost me like two hours of debugging ahhhhhhhh. I had originally used grid[:]
+        #then I tried list(grid) then grid.copy() but it took me forever to find the source
+        #I will forever remeber that when you copy a list it refrences the same place in memory
+        #idk why grid[:] didn't work bc I would have never had any problems
+        #if you have have an answer for me I would love to know Dr. Ritchey ~ Paul
         piece.x = pos[i][0]
         piece.y = pos[i][1]
         loc = convertShape(piece)
         for k in range(4):
             x, y = loc[k]
             grid1[y][x] = 1
-        score = rate(grid1)
+        score = rate(grid1,loc)
         if score < bestscore:
             bestscore = score
             best_x = pos[i][0]
@@ -280,6 +289,7 @@ def best_rotation(grid, piece):
     tests the best positions from each rotation and decides which rotation is the best.
     returns the rotation and x position
     '''
+    #piece = Piece(x, y, shape)
     rotation = 0
     best_score = 100000
     num, x = best_position(piece, grid)
@@ -289,7 +299,11 @@ def best_rotation(grid, piece):
     num2, x2 = best_position(piece, grid) 
     piece.rotation += 1
     num3, x3 = best_position(piece, grid) 
-    rotations = [num,0, x] + [num1, 1, x1] + [num2, 2, x2] + [num3, 3, x3]
+    rotations = []
+    rotations.append([num,0, x])
+    rotations.append([num1,1, x1])
+    rotations.append([num2,2, x2])
+    rotations.append([num2,3, x3])
     for i in range(4):
         if rotations[i][0] < best_score:
             rotation = rotations[i][1]
@@ -305,22 +319,22 @@ def run(grid, x, y, shape):
     p = Piece(x, y, shape) #create same piece from JTetris
     global counter
     counter += 1
-    if counter < 3: #slow the brain down
+    if counter < 100: #slow the brain down, w/out slowing it down the window was crashing 
         return []
     counter = 0
-    rotation = 0
-    print(grid)
+    rotation = p.rotation
     grid = sort(grid)
-    print(grid)
-    r, position = best_position(p,grid)
+    
+    r, position = best_rotation(grid, p)
 
     if r != rotation:
         e = Event(pygame.KEYDOWN, pygame.K_UP)
     elif position < x:
-        e = Event(pygame.KEYDOWN, pygame.K_RIGHT)
-    elif position > x:
         e = Event(pygame.KEYDOWN, pygame.K_LEFT)
+    elif position > x:
+        e = Event(pygame.KEYDOWN, pygame.K_RIGHT)
     else:
         e = Event(pygame.KEYDOWN, pygame.K_DOWN)
-        
+
+   # e = Event(pygame.KEYDOWN, pygame.K_DOWN)
     return [e]
